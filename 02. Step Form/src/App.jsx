b6e903367b199be5d26799442b06e5474ae07b89
Mirka <mirkamedaglini@hotmail.it>
2023-10-07 23:09:50 +0200

    Project 2

---
+++ 02. Step Form/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MultiStepForm from './components/MultiStepForm'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app-container'>
+      <h1>Progress Form</h1>
+      <MultiStepForm />
+    </div>
+  )
+}
+
+export default App
